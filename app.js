f3715fa4e4aa38c48e76509c94e6f222f685a354
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2018-02-18 16:17:18 -0500

    ready to deploy?

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var mongojs = require('mongojs');
+var db = mongojs('localhost:27017/myDB', ['account', 'playerStats']);
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
@@ -14,35 +17,34 @@ class Ball {
   constructor() {
     this.x = 512;
     this.y = 300;
-    this.radius = 10;
+    this.radius = 5;
     this.color = "#2FBF86";
     this.velocityX = 0;
     this.velocityY = 0;
-    this.maxSpeed = 100;
-    this.friction = .99;
+    this.maxSpeed = 50;
+    this.friction = 1;
   }
-  reset() {
+  restart() {
     this.x = 512;
     this.y = 300;
     this.velocityX = 0;
     this.velocityY = 0;
   }
+  reset() {
+    this.x = Math.floor(Math.random() * 1016);
+    this.y = Math.floor(Math.random() * 595);
+    this.velocityX = Math.floor(Math.random() * 5);
+    this.velocityY = Math.floor(Math.random() * 5);
+  }
   update() {
     this.updatePosition();
-    this.checkGoal();
   }
   updatePosition(){
-    for (var i in playerList){
-      if (playerList[i].x < this.x + 5 && playerList[i].x + 16 > this.x &&
-          playerList[i].y < this.y + 5 && playerList[i].y + 16 > this.y) {
-        if (playerList[i].velocityX == 0 && playerList[i].velocityY == 0) {
-          this.velocityX = -this.velocityX;
-          this.velocityY = -this.velocityY;
-        }
-        else {
-          this.velocityX = playerList[i].velocityX*2;
-          this.velocityY = playerList[i].velocityY*2;
-        }
+    for (var i in Player.list){
+      if (Player.list[i].x < this.x + 5 && Player.list[i].x + 16 > this.x &&
+          Player.list[i].y < this.y + 5 && Player.list[i].y + 16 > this.y) {
+            this.reset();
+            Player.list[i].captures++;
       }
     }
 
@@ -51,66 +53,109 @@ class Ball {
     this.velocityX *= this.friction;
     this.x += this.velocityX;
 
-    if (this.x + this.velocityX > 1014) {
+    if (this.x + this.velocityX > 1019) {
       this.velocityX = -this.velocityX;
     } else if (this.x + this.velocityX < 5){
       this.velocityX = -this.velocityX;
     }
-    if (this.y + this.velocityY >= 590) {
+    if (this.y + this.velocityY >= 595) {
       this.velocityY = -this.velocityY;
     } else if (this.y + this.velocityY < 5) {
       this.velocityY = -this.velocityY;
     }
 
-    if (this.x >= 1014) {
-      this.x = 1014;
+    if (this.x >= 1019) {
+      this.x = 1019;
     } else if (this.x < 0){
       this.x = 0;
     }
-    if (this.y >= 590) {
-      this.y = 590;
+    if (this.y >= 595) {
+      this.y = 595;
     } else if (this.y < 0) {
       this.y = 0;
     }
   }
+};
 
-  checkGoal() {
-    if (this.x >= 1008) {
-      console.log("yup");
-      if (this.y >= 256 && this.y <= 320) {
-        // goal. reset position
-        this.reset();
-        score['b'] += 1;
-        console.log("goal");
+class Debris {
+  constructor() {
+    this.x = Math.floor(Math.random() * 1008);
+    this.y = 8;
+    this.velocityX = 0//Math.floor(Math.random() * 4) + 2;
+    this.velocityY = Math.floor(Math.random() * 4) + 2;
+  }
+  reset() {
+    this.x = Math.floor(Math.random() * 1016);
+    this.y = 8
+    this.velocityX = 0
+    this.velocityY = Math.floor(Math.random() * 5);
+  }
+  update() {
+    this.updatePosition();
+  }
+  updatePosition() {
+    for(var i in Player.list) {
+      if (Player.list[i].x < this.x + 16 && Player.list[i].x + 16 > this.x &&
+          Player.list[i].y < this.y + 16 && Player.list[i].y + 16 > this.y) {
+            this.reset();
+            Player.list[i].captures--;
+            Player.list[i].reset();
       }
     }
-    if (this.x <= 8) {
-      console.log("no.");
-      if (this.y >= 256 && this.y <= 320) {
-        // goal. reset position
-        this.reset();
-        this.velocityX =
-        score['r'] += 1;
-        console.log("goal");
-      }
+    this.y += this.velocityY;
+    this.x += this.velocityX;
+
+    if (this.x + this.velocityX > 1008) {
+      this.velocityX = -this.velocityX;
+    } else if (this.x + this.velocityX < 5){
+      this.velocityX = -this.velocityX;
+    }
+    if (this.y + this.velocityY >= 584) {
+      this.velocityY = -this.velocityY;
+    } else if (this.y + this.velocityY < 5) {
+      this.velocityY = -this.velocityY;
+    }
+
+    if (this.x >= 1008) {
+      this.x = 1008;
+    } else if (this.x < 0){
+      this.x = 0;
+    }
+    if (this.y >= 584) {
+      this.y = 595;
+    } else if (this.y < 0) {
+      this.y = 0;
     }
   }
 }
 
 class Player {
-  constructor(id, team) {
-    this.team = team;
-    if (this.team == "blue"){
-      this.x = 312;
-      this.y = 300;
-      this.color = "#4858FE";
-    }
-    else {
-      this.x = 712;
-      this.y = 300;
-      this.color = "#f44256";
-    }
+  constructor(id, name) {
     this.id = id;
+    this.name = name;
+    if (playerCount == 1) {
+      this.color = "blue";
+      this.x = 250;
+      this.y = 200;
+    }
+    if (playerCount == 2) {
+      this.color = "red";
+      this.x = 750;
+      this.y = 200;
+    }
+    if (playerCount == 3) {
+      this.color = "teal";
+      this.x = 250;
+      this.y = 400;
+    }
+    if (playerCount == 4){
+      this.color = "purple";
+      this.x = 750;
+      this.y = 400;
+    }
+
+    this.startX = this.x;
+    this.startY = this.y;
     this.size = 16;
     this.number = '' + Math.floor(Math.random() * 10);
     this.leftPressed = false;
@@ -119,11 +164,31 @@ class Player {
     this.downPressed = false;
     this.velocityX = 0;
     this.velocityY = 0;
-    this.maxSpeed = 3;
-    this.friction = .85;
+    this.maxSpeed = 5;
+    this.friction = .975;
+    this.captures = 0;
+    this.ready = false;
+    Player.list[id] = this;
   }
-
+  reset() {
+    this.x = this.startX;
+    this.y = this.startY;
+    this.velocityX = 0;
+    this.velocityY = 0;
+  }
+  update() {
+    this.updatePosition();
+  }
   updatePosition(){
+    for(var i in Player.list) {
+      if (this.id != Player.list[i].id) {
+        if (Player.list[i].x < this.x + 16 && Player.list[i].x + 16 > this.x &&
+            Player.list[i].y < this.y + 16 && Player.list[i].y + 16 > this.y) {
+              this.velocityX = -this.velocityX;
+              this.velocityY = -this.velocityY;
+        }
+      }
+    }
     if (this.leftPressed) {
       if (this.velocityX > -this.maxSpeed) {
         this.velocityX--;
@@ -172,36 +237,9 @@ class Player {
     }
   }
 };
-
-var socketList = {}
-var playerList = {}
-var ball = new Ball();
-var score = {
-  'b': 0,
-  'r': 0
-};
-
-goalPositions = []
-count = 0;
-var io = require('socket.io')(serv, {});
-io.sockets.on('connection', function(socket) {
-  socket.id = count;
-  socketList[count] = socket;
-  count++;
-  if (count % 2 == 0){
-    team = "blue"
-  }
-  else {
-    team = "red"
-  }
-  var player = new Player(socket.id, team);
-  playerList[socket.id] = player;
-
-  socket.on('disconnect', function() {
-    delete socketList[socket.id];
-    delete playerList[socket.id];
-  });
-
+Player.list = {}
+Player.onConnect = function(socket, name){
+  var player = new Player(socket.id, name);
   socket.on('keyPress', function(data) {
     if (data.inputId === 'left') {
       player.leftPressed = data.state;
@@ -213,27 +251,273 @@ io.sockets.on('connection', function(socket) {
       player.downPressed = data.state;
     }
   });
+}
+Player.onDisconnect = function(socket) {
+  /*
+  var player = Player.list[socket.id];
+  setPlayerInactive(player, function(){
+    db.account.update({username: player.name}, {$set:{active: false}});
+  });*/
+  delete Player.list[socket.id]
+  if (Object.keys(Player.list).length < 2) {
+    gameover = true;
+  }
+}
+Player.reset = function() {
+  for(var i in Player.list) {
+    Player.list[i].reset();
+    Player.list[i].captures = 0;
+  }
+}
+var users = {}
+var inGame = false;
+var socketList = {}
+var playerCount = 0;
+var ball = new Ball();
+var debrisList = [];
+var gameover = false;
+var winner = "";
+for(var i = 0; i < 10; i++) {
+  debrisList.push(new Debris());
+}
+
+var playerLookup = function(p) {
+  for (var i in Player.list) {
+    if (p == Player.list[i].name){
+      return Player.list[i];
+    }
+  }
+  return false;
+};
+
+var checkGameStart = function() {
+  var playersReady = 0;
+  playing = [];
+  for (var i in Player.list) {
+    if (Player.list[i].ready) {
+      playing.push(Player.list[i])
+      playersReady++;
+    }
+  }
+  if (playersReady >= 2) {
+    for (var i in socketList) {
+      var socket = socketList[i];
+      socket.emit('startingGame');
+    }
+  }
+};
+
+var resetGame = function() {
+  Player.reset();
+  ball.restart();
+  debrisList = [];
+  gameover = false;
+  winner = "";
+  for(var i = 0; i < 10; i++) {
+    debrisList.push(new Debris());
+  }
+  for (var i in socketList) {
+    var socket = socketList[i];
+    socket.emit('clearInfo');
+  }
+  startGame();
+};
+
+var sendPlayerList = function() {
+  pack = [];
+  for (var i in Player.list) {
+    var player = Player.list[i];
+    pack.push({
+      name: player.name,
+    })
+  }
+  for(var i in socketList) {
+    var socket = socketList[i];
+    socket.emit('activePlayers', pack);
+  }
+};
+
+var setPlayerInactive = function(data, callback) {
+  try {
+    db.account.find({username: data.name}, function(err, res){
+      if (res.length > 0) {
+        callback();
+      }
+    });
+  } catch(error) {
+    console.log(error);
+  }
+}
+var isValidPassword = function(data, callback) {
+  db.account.find({username: data.username, password: data.password}, function(err, res){
+    if (res.length > 0) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
+var isUsernameTaken = function(data, callback) {
+  db.account.find({username: data.username}, function(err, res){
+    if (res.length > 0) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
+
+var addUser = function(data, callback) {
+  db.account.insert({username: data.username, password: data.password}, function(err){
+    callback();
+  });
+}
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket) {
+  socket.id = Math.random();
+  socketList[socket.id] = socket;
+  socket.on('signIn', function(data) {
+    if (playerCount < 4) {
+      playerCount++;
+      Player.onConnect(socket, data.username);
+    }
+    socket.emit('signInResponse', {success: true});
+    //socket.emit('playerConnected', {name: data.username});
+    /*
+    isValidPassword(data, function(res) {
+      if (res) {
+        if (playerCount < 4) {
+          playerCount++;
+          Player.onConnect(socket, data.username);
+        }
+        db.account.update({username: data.username}, {$set:{active: true}});
+        socket.emit('signInResponse', {success: true});
+        socket.emit('playerConnected', {name: data.username});
+        sendPlayerList();
+      } else {
+        socket.emit('signInResponse', {success: false});
+      }
+    });*/
+  });
+
+  /*
+  socket.on('signUp', function(data) {
+    isUsernameTaken(data, function(res) {
+      if (res) {
+        socket.emit('signUpResponse', {success: false});
+      } else {
+        addUser(data, function(){
+          db.playerStats.insert({username: data.username, wins: 0, losses: 0});
+          socket.emit('signUpResponse', {success: true});
+        });
+      }
+    });
+  });*/
+
+  socket.on('activePlayerList', function() {
+    sendPlayerList();
+  });
+
+  socket.on('startGame', function() {
+    for(var i in socketList) {
+      var socket = socketList[i];
+      socket.emit('removeStartButton');
+    }
+    startGame();
+  });
+
+  socket.on('newGame', function() {
+    resetGame();
+  });
+
+  socket.on('toLobby', function(data) {
+    player = playerLookup(data.username);
+    if (player) {
+      player.ready = false;
+      readyCount = 0;
+      for (var i in Player.list) {
+        if (Player.list[i].ready) {
+          readyCount++;
+        }
+      }
+      if (readyCount < 2) {
+        inGame = false;
+        for(var i in socketList) {
+          var socket = socketList[i];
+          socket.emit("endingGame");
+        }
+      }
+    }
+  });
+
+  socket.on('playerReady', function(data) {
+    player = playerLookup(data.username);
+    if (player) {
+      player.ready = true;
+      checkGameStart();
+    }
+  });
+  socket.on('disconnect', function() {
+    playerCount--;
+    delete socketList[socket.id];
+    Player.onDisconnect(socket);
+  });
 });
 
-setInterval(function() {
+var gameLoop = function() {
   var pack = [];
-  ball.update();
-  for(var i in playerList) {
-    var player = playerList[i];
-    player.updatePosition();
+  var debrisPack = [];
+  for (var i in debrisList) {
+    var debris = debrisList[i];
+    debris.update();
+    debrisPack.push({
+      x: debris.x,
+      y: debris.y
+    });
+  }
+  for(var i in Player.list) {
+    var player = Player.list[i];
+    if (player.captures >= 10) {
+      winner = player.name;
+      gameover = true;
+    }
+    ball.update();
+    player.update();
     pack.push({
       x: player.x,
       y: player.y,
       color: player.color,
       size: player.size,
+      captures: player.captures,
       ballX: ball.x,
       ballY: ball.y,
       ballRadius: ball.radius,
-      score: score,
+      debris: debrisPack,
     });
   }
   for(var i in socketList) {
     var socket = socketList[i];
-    socket.emit('newPositions', pack);
+    if (!gameover) {
+      socket.emit('newPositions', pack);
+    } else {
+      socket.emit('gameover',{winner});
+    }
   }
-}, 1000/60);
+  if (gameover) {
+    inGame = false;
+    clearInterval(this);
+  }
+};
+
+
+var startGame = function() {
+  console.log("initializing game");
+  inGame = true;
+  setInterval(gameLoop, 1000/60);
+}
+
+var stopGame = function() {
+  console.log("stopping game");
+  inGame = false;
+  clearInterval(gameLoop);
+}
--- client/js/main.js
+++ client/js/main.js
@@ -1,31 +1,167 @@
 var socket = io();
 
+// Sign in Window
+var signIn = document.getElementById("signIn");
+var signInUsername = document.getElementById("signIn-username");
+var signInPassword = document.getElementById("signIn-password");
+var signInButton = document.getElementById("signIn-signIn");
+var signUpButton = document.getElementById("signIn-signUp");
+var username = signInUsername.value;
+var gameDiv = document.getElementById('gameDiv');
+
+signInButton.onclick = function() {
+  socket.emit("signIn",{username:signInUsername.value});
+};
+
+/* Removed because this game doesnt need a database or login system right now
+signInButton.onclick = function() {
+  socket.emit("signIn",{username:signInUsername.value, password:signInPassword.value});
+};
+
+signUpButton.onclick = function() {
+  socket.emit("signUp",{username:signInUsername.value, password:signInPassword.value});
+};*/
+
+socket.on("signInResponse", function(data) {
+  if (data.success) {
+    signIn.style.display = "none";
+    lobbyDiv.style.display = "inline-block";
+    username = signInUsername.value;
+    displayPlayer();
+    socket.emit('activePlayerList');
+  } else {
+    alert("Sign In Unsuccessful");
+  }
+});
+
+/*
+socket.on("signUpResponse", function(data) {
+  if (data.success) {
+    alert("Sign Up Successful");
+  } else {
+    alert("Sign Up Unsuccessful");
+  }
+});
+*/
+
+// lobby
+lobbyDiv = document.getElementById("lobbyDiv");
+lobby = document.getElementById('lobby');
+findGameButton = document.getElementById('findGame');
+
+findGameButton.onclick = function() {
+  var div = document.createElement("div");
+  div.innerHTML += "Waiting for 1 more player to search for game.";
+  lobbyDiv.appendChild(div);
+  socket.emit('playerReady', {username});
+}
+
+var displayPlayer = function() {
+  var div = document.createElement('div');
+  div.innerHTML += "Logged in as <strong>" + username +"</strong><br>";
+  div.innerHTML += "<hr><br>Players in lobby <br><hr>";
+  lobby.appendChild(div);
+};
+
+playerDiv = document.getElementById('players');
+socket.on('activePlayers', function(data) {
+  playerDiv.innerHTML = "";
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement('div');
+    div.innerHTML += "<strong>"+data[i].name+"</strong>";
+    playerDiv.appendChild(div);
+  }
+});
+
+
+socket.on('startingGame', function(){
+  lobbyDiv.style.display = "none";
+  gameDiv.style.display = "inline-block";
+});
+
+// Game
+var startButton = document.getElementById("startGame");
+var newButton = document.getElementById("newGame");
+//var lobbyButton = document.getElementById("toLobby");
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
+var gameStarted = false;
+
+startButton.onclick = function() {
+  socket.emit('startGame');
+};
+
+socket.on('removeStartButton', function(){
+  startButton.style.display = "none";
+});
+
+newButton.onclick = function() {
+  socket.emit('newGame');
+};
+
+/*
+lobbyButton.onclick = function() {
+  gameDiv.style.display = "none";
+  lobbyDiv.style.display = "inline-block";
+  socket.emit("toLobby", {username: username});
+  socket.emit('activePlayerList');
+};
+
+socket.on('endingGame', function() {
+  gameDiv.style.display = "none";
+  lobbyDiv.style.display = "inline-block";
+  socket.emit('activePlayerList');
+});
+*/
 
 socket.on('newPositions', function(data) {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  // write score on screen
-  context.fillStyle = "black";
-  context.font = "14px Arial";
-  context.textAlign = "center";
-  context.textBaseline = "top";
-  context.fillText("Blue: " + data[0].score['b'] + " Red: " + data[0].score['r'], 50, 10);
+  try {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // write score on screen
+    context.fillStyle = "black";
+    context.font = "14px Arial";
+    context.textAlign = "center";
+    context.textBaseline = "top";
 
-  // draw ball
-  context.beginPath();
-  context.arc(data[0].ballX, data[0].ballY, data[0].ballRadius, 0, Math.PI*2);
-  context.fillStyle = "#2FBF86";
-  context.fill();
-  context.closePath();
+    // draw ball
+    context.beginPath();
+    context.arc(data[0].ballX, data[0].ballY, data[0].ballRadius, 0, Math.PI*2);
+    context.fillStyle = "black";
+    context.fill();
+    context.closePath();
 
-  context.fillStyle = "#333";
-  context.fillRect(0, 256, 8, 64);
-  context.fillRect(1016, 256, 8, 64);
-  for(var i = 0; i < data.length; i++) {
-    context.fillStyle = data[i].color;
-    context.fillRect(data[i].x, data[i].y, data[i].size, data[i].size);
-  }
+    captures = "";
+    for(var i = 0; i < data.length; i++) {
+      context.fillStyle = data[i].color;
+      context.fillRect(data[i].x, data[i].y, data[i].size, data[i].size);
+      captures += data[i].color + ": " + data[i].captures + " ";
+
+      for(var j = 0; j < data[i].debris.length; j++) {
+        context.fillStyle = "brown";
+        context.fillRect(data[i].debris[j].x, data[i].debris[j].y, 16, 16);
+      }
+    }
+    context.fillStyle = "black";
+    context.fillText(captures, canvas.width / 2, 10);
+  } catch (error) {
+      signIn.style.display = "none";
+      lobbyDiv.style.display = "inline-block";
+      username = signInUsername.value;
+      displayPlayer();
+      socket.emit('activePlayerList');
+      console.log(error);
+    }
+});
+
+socket.on('clearInfo', function() {
+  document.getElementById('info').innerHTML = "";
+});
+
+socket.on('gameover', function(data) {
+  startButton.style.display = "none";
+  newButton.style.display = "inline-block";
+  var info = document.getElementById('info');
+  info.innerHTML += data.winner + " won the game!";
 });
 
 document.onkeydown = function(event) {
