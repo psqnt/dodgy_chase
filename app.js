6a2f858eaed9e3711b7012354d0bc518a0be809c
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2018-02-18 16:26:08 -0500

    ready

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var mongojs = require('mongojs');
-var db = mongojs('localhost:27017/myDB', ['account', 'playerStats']);
+//var mongojs = require('mongojs');
+var db = null //mongojs('localhost:27017/myDB', ['account', 'playerStats']);
 
 var express = require('express');
 var app = express();
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log('server started');
 
 class Ball {
